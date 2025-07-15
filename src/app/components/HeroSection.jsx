044d172c45c157a7b05ad1a40b56bef8f438f2b6
Edudrolhe <edudrolhe@gmail.com>
2025-07-15 16:42:33 -0300

    Atualização do deploy

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
         <div className="col-span-7 place-self-center text-center sm:text-left justify-self-start">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-br from-red-500 via-blue-500 to-purple-600">
-              {"Hello, I&apos;m "}
+              {"Hello World! "}
             </span>
             <br />
             <TypeAnimation
